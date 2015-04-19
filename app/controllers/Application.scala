ecd8bdf732bdd63fa55e6226d6a42e8fc807224e
pamu <nagarjuna.pamu@gmail.com>
2015-04-20 01:25:42 +0530

    db code added

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,11 +3,13 @@ package controllers
 import play.api.mvc.{Action, Controller}
 
 object Application extends Controller {
+
   def index = Action {
     Ok(views.html.index("Hello :)"))
   }
-  def client(id: Long, source: String, destination: String) = Action {
 
+  def client(id: Long, source: String, destination: String) = Action {
     Ok("")
   }
+
 }
--- app/models/Db.scala
+++ app/models/Db.scala
@@ -1,8 +1,29 @@
 package models
 
+import java.net.URI
+
 /**
  * Created by pamu on 20/4/15.
  */
-object Db {
+object ProductionDb {
+  import scala.slick.driver.PostgresDriver.simple._
 
+  val uri = new URI("postgres://gsqbgrxvpyadee:KmgebP3eQ8BqgG4k2OnVPVme-E@" +
+    "ec2-107-22-173-230.compute-1.amazonaws.com:5432/d778l22q1rk5mg")
+
+  val username = uri.getUserInfo.split(":")(0)
+
+  val password = uri.getUserInfo.split(":")(1)
+
+  lazy val db = Database.forURL(
+    driver = "org.postgresql.Driver",
+    url = "jdbc:postgresql://" + uri.getHost + ":" + uri.getPort + uri.getPath, user = username,
+    password = password)
+}
+
+object SampleDb {
+  import scala.slick.driver.MySQLDriver.simple._
+
+  lazy val db = Database.forURL(url = "jdbc:mysql://localhost:3306/demo", driver = "com.jdbc.mysql.Driver",
+    user = "root", password = "root")
 }
