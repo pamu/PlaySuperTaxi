8531776d58f1ceeddd3ab4f970f783a5ce3961ec
pamu <nagarjuna.pamu@gmail.com>
2015-04-20 01:48:47 +0530

    dashboard

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,14 +2,20 @@ package controllers
 
 import play.api.mvc.{Action, Controller}
 
+import scala.concurrent.Future
+
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Hello :)"))
   }
 
-  def client(id: Long, source: String, destination: String) = Action {
-    Ok("")
+  def client(id: Long, source: String, destination: String) = Action.async {
+    Future {
+
+      Ok("saved")
+    }
+
   }
 
 }
