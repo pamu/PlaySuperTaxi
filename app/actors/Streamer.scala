cdc21f5ffef771b1a147892ee70309c64334ae3a
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-04-22 15:41:54 +0530

    added js

--- app/actors/Streamer.scala
+++ app/actors/Streamer.scala
@@ -1,8 +1,29 @@
 package actors
 
+import akka.actor.{ActorLogging, Actor}
+import play.api.libs.iteratee.Concurrent
+import play.api.libs.json.JsValue
+
 /**
  * Created by pnagarjuna on 22/04/15.
  */
 object Streamer {
+  case object ClientRequestStream
+  case object DriverStream
+  case class Push(jsValue: JsValue)
+}
 
+class Streamer extends Actor with ActorLogging {
+  import Streamer._
+  val (in, out) = Concurrent.broadcast[JsValue]
+  override def receive = {
+    case Push(jsValue) => out push jsValue
+    case ClientRequestStream =>
+      sender ! in
+      log.info("Got a ClientRequestStream request")
+    case DriverStream =>
+      sender ! in
+      log.info("Got a DriverRequestStream request")
+    case strange => log.info("strange message {} of type {}", strange, strange getClass)
+  }
 }
